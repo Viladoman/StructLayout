dec9361a2ceae7423113cc47588b1fa42eff9ad9
Ramon Viladomat <ramon.viladomat@gmail.com>
2020-11-16 18:59:21 -0500

    Added CMake Test Project

---
+++ TestProjects/CMake/src/main.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+
+struct Empty{};
+
+struct EmptyBase : public Empty
+{ 
+	float a; 
+};
+
+template<typename T>
+struct TemplatedStruct
+{ 
+	T t;
+};
+
+struct Virtual
+{ 
+	virtual void foo(){}
+
+	EmptyBase b;
+};
+
+struct VirtualChild : public Virtual
+{ 
+	TemplatedStruct<double> t; 
+
+#ifdef TARGET_DEBUG
+	double d; 
+	double e;
+#endif
+};
+
+
+int main()
+{ 
+	TemplatedStruct<float> temp; 
+
+	printf("HELLO STRUCT LAYOUT!\n");
+}
